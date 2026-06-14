b6fe74691107ae614369aa30785f0fe969b5f7b5
hashem <hashem.sf@gmail.com>
2026-06-14 19:06:19 +0300

    adding ecto db

--- lib/heads_up_web/controllers/live/incident_live/index.ex
+++ lib/heads_up_web/controllers/live/incident_live/index.ex
@@ -2,13 +2,22 @@ defmodule HeadsUpWeb.IncidentLive.Index do
   use HeadsUpWeb, :live_view
 
   import HeadsUpWeb.CustomComponents
+  alias HeadsUp.Incidents
+  alias HeadsUp.Incidents.Incident
 
   def mount(_params, _session, socket) do
     socket =
       assign(socket,
-        incidents: HeadsUp.Incidents.list_incidents(),
         page_title: "Incidents"
       )
+      |> stream(:incidents, Incidents.list_incidents())
+
+    socket =
+      attach_hook(socket, :log_stream, :after_render, fn
+        socket ->
+          IO.inspect(socket)
+          socket
+      end)
 
     {:ok, socket}
   end
@@ -24,17 +33,24 @@ defmodule HeadsUpWeb.IncidentLive.Index do
           </:tagline>
         </.head_line>
 
-        <div class="incidents">
-          <.incident_card :for={incident <- @incidents} incident={incident} />
+        <div class="incidents" id="incidents" phx-update="stream">
+          <.incident_card
+            :for={{dom_id, incident} <- @streams.incidents}
+            incident={incident}
+            id={dom_id}
+          />
         </div>
       </div>
     </Layouts.app>
     """
   end
 
+  attr :incident, Incident, required: true
+  attr :id, :string, required: true
+
   def incident_card(assigns) do
     ~H"""
-    <.link navigate={~p"/incidents/#{@incident.id}"}>
+    <.link navigate={~p"/incidents/#{@incident.id}"} id={@id}>
       <div class="card">
         <img src={@incident.image_path} />
         <h2>{@incident.description}</h2>
